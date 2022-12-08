7cfed27120dee784f604da7ccb9a02db30012262
Anita <anitakosman@hotmail.com>
2022-12-08 17:59:42 +0100

    Refactor day 6

--- src/main/kotlin/Day06.kt
+++ src/main/kotlin/Day06.kt
@@ -1,5 +1,5 @@
 fun main() {
     val input = getInput("day6").readText()
-    println(input.windowed(4).map { it.toList() }.withIndex().first { it.value.distinct() == it.value }.index + 4)
-    println(input.windowed(14).map { it.toList() }.withIndex().first { it.value.distinct() == it.value }.index + 14)
+    println(input.windowed(4).map { it.toList() }.indexOfFirst { it.distinct() == it } + 4)
+    println(input.windowed(14).map { it.toList() }.indexOfFirst { it.distinct() == it } + 14)
 }
